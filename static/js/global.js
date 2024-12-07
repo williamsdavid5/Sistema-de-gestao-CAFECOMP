2232850bca183897bf463b95dc2a593537652d35
David Duarte (Williams) <davidduart04@gmail.com>
2024-12-07 15:11:42 -0300

    criacao dos menus superior e lateral (padrao para todas as paginas)

---
+++ static/js/global.js
@@ -0,0 +1,13 @@
+//configurações globais do menu lateral
+const menuLateralIcon = document.getElementById('menuLateralIcon');
+const menuLateralIcon2 = document.getElementById('menuLateralIcon2');
+const menuLateral = document.getElementById('menuLateral');
+
+menuLateralIcon.addEventListener('click', () => {
+    menuLateral.classList.add('show');
+});
+
+menuLateralIcon2.addEventListener('click', () => {
+    menuLateral.classList.remove('show');
+});
+
