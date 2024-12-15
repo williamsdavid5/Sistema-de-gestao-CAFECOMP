a66d03e6654984c22a6da6d94d53b63a7226f4c3
David Duarte (Williams) <davidduart04@gmail.com>
2024-12-15 17:37:15 -0300

    correcao tela portal transparencia

--- static/js/portalTransparencia.js
+++ static/js/portalTransparencia.js
@@ -1,33 +1,37 @@
-const slider = document.getElementById("deslizante");
-const box = document.getElementById("conteudo");
-
-// Atualiza a opacidade do elemento sempre que o valor do slider mudar
-slider.addEventListener("input", () => {
-    const opacityValue = (slider.value / 100); // Pega o valor do slider
-    box.style.opacity = opacityValue; // Define a opacidade no estilo inline
-});
-
 import { verificarLoginLocal } from './global.js';
 
 document.addEventListener('DOMContentLoaded', () => {
+    // Verificação de login e redirecionamento caso o usuário não esteja logado
     if (!verificarLoginLocal()) {
         console.warn("Usuário não está logado. Redirecionando para /login");
-        window.location.href = '/login';  // Redireciona para a página de login (rota login_route)
+        window.location.href = '/login';  // Redireciona para a página de login
     }
-});
 
-document.addEventListener('DOMContentLoaded', () => {
+    // Controle do slider de transparência
+    const slider = document.getElementById("deslizante");
+    const box = document.getElementById("conteudo");
+
+    // Atualiza a opacidade do elemento sempre que o valor do slider mudar
+    slider.addEventListener("input", () => {
+        const opacityValue = (slider.value / 100); // Pega o valor do slider
+        box.style.opacity = opacityValue; // Define a opacidade no estilo inline
+    });
+
+    // Controle da visibilidade da janela de registrar alteração de caixa
     const botaoRegistrarCaixa = document.getElementById('botaoRegistrarCaixa');
     const registrarCaixa = document.getElementById('registrarCaixa');
     const botaoCancelar = document.getElementById('botaoCancelar');
 
-    // Mostrar a div registrarCaixa ao clicar no botão botaoRegistrarCaixa
-    botaoRegistrarCaixa.addEventListener('click', () => {
-        registrarCaixa.style.display = 'flex';  // Mostra a div
-    });
+    // Verifica se os elementos existem antes de adicionar os eventos
+    if (botaoRegistrarCaixa && registrarCaixa && botaoCancelar) {
+        // Mostrar a div registrarCaixa ao clicar no botão botaoRegistrarCaixa
+        botaoRegistrarCaixa.addEventListener('click', () => {
+            registrarCaixa.style.display = 'flex';  // Mostra a div
+        });
 
-    // Esconder a div registrarCaixa ao clicar no botão botaoCancelar
-    botaoCancelar.addEventListener('click', () => {
-        registrarCaixa.style.display = 'none';  // Esconde a div
-    });
+        // Esconder a div registrarCaixa ao clicar no botão botaoCancelar
+        botaoCancelar.addEventListener('click', () => {
+            registrarCaixa.style.display = 'none';  // Esconde a div
+        });
+    }
 });
