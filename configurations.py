8e65b2471174b9f0e72db9cfc260db9f529bdc89
Johsep06 <jose.r.maciel@outlook.com>
2024-12-15 15:12:13 -0300

    erro de nomeclaturas corrigido

--- configurations.py
+++ configurations.py
@@ -4,7 +4,7 @@ from routes.cadastro import cadasto_route
 from routes.chamados import chamados_route
 from routes.noticias import noticias_route
 from routes.noticia_pagina import noticia_pagina_route
-from routes.portal_transparencia import portal_transparencia_route
+from routes.portal_transparencia import portal_tranparencia_route
 
 def configure_all(app):
     configure_routes(app)
@@ -16,4 +16,4 @@ def configure_routes(app):
     app.register_blueprint(chamados_route, url_prefix='/chamados')
     app.register_blueprint(noticias_route, url_prefix='/noticias')
     app.register_blueprint(noticia_pagina_route, url_prefix='/noticias-pagina')
-    app.register_blueprint(portal_transparencia_route, url_prefix='/portal-transparencia')
+    app.register_blueprint(portal_tranparencia_route, url_prefix='/portal-transparencia')
